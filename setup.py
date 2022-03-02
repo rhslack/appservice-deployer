572a8e2c95952faf3e86c6688899a3231dbfd8c5
Fabio Felici <fabio.felici96c@gmail.com>
2022-03-02 13:57:30 +0000

    release v0.0.3.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup_kwargs = {
     'name': 'appservice-deployer',
-    'version': '0.0.3.2',
+    'version': '0.0.3.3',
     'description': 'Azure App Service file deployer',
     'long_description': long_description,
     'license': 'MIT',
